1baf237b7307b1a9d4aa577675ab9c3db8e47f4b
Feng SUN <42902314+jamessunf@users.noreply.github.com>
2019-09-23 21:35:42 -0400

    Android_Lab3:link login

--- app/src/main/java/com/example/android_lab1/MainActivity.java
+++ app/src/main/java/com/example/android_lab1/MainActivity.java
@@ -9,9 +9,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_main_grid);
         //setContentView(R.layout.activity_main_linear);
        // setContentView(R.layout.activity_main_relative);
+        setContentView(R.layout.activity_main_login);
     }
 }
