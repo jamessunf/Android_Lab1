173dd8793931e5dccd7650aa76123ef0f957d19e
Feng SUN <42902314+jamessunf@users.noreply.github.com>
2019-09-28 10:56:21 -0400

    Android_Lab3:add shared preferences

--- app/src/main/java/com/example/android_lab1/MainActivity.java
+++ app/src/main/java/com/example/android_lab1/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.android_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    Button btn2;
+    EditText edtEmail;
+    EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +26,55 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main_linear);
        // setContentView(R.layout.activity_main_relative);
         setContentView(R.layout.activity_main_login);
+
+        btn = (Button) findViewById(R.id.btn_login);
+        btn2 = (Button) findViewById(R.id.btn_login2);
+
+        edtEmail = (EditText) findViewById(R.id.login_email);
+        edtPassword = (EditText) findViewById(R.id.login_password);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               // Toast.makeText(getApplicationContext(),"It's magic!", Toast.LENGTH_LONG).show();
+                saveInfo(view);
+
+
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayData(view);
+            }
+        });
+
+
+    }
+
+    private void saveInfo(View view) {
+
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("userEmail", edtEmail.getText().toString());
+        editor.putString("userPassword",edtPassword.getText().toString());
+        editor.apply();
+
+        Toast.makeText(this,"Saved!" ,Toast.LENGTH_LONG).show();
+    }
+    public void displayData(View view){
+
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+
+        String email = sharedPref.getString("userEmail","");
+        String password = sharedPref.getString("userPassword","");
+        Toast.makeText(this,email+ ","+ password,Toast.LENGTH_LONG).show();
+
+
     }
 }
