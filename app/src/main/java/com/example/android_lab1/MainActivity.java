983caf0a8d7cfcd230a487a9bd4be4caf31008bb
Feng SUN <42902314+jamessunf@users.noreply.github.com>
2019-09-30 16:14:13 -0400

    lab2 test 0909

--- app/src/main/java/com/example/android_lab1/MainActivity.java
+++ app/src/main/java/com/example/android_lab1/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_grid);
         //setContentView(R.layout.activity_main_linear);
         //setContentView(R.layout.activity_main_relative);
+        ////
     }
 }
